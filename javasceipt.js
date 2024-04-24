b08737de1727143afd6286e857beff690f44a75b
abushahid81 <abushahidsardar@gmail.com>
2024-04-25 03:02:07 +0530

    html css

---
+++ javasceipt.js
@@ -0,0 +1,29 @@
+let typingEffect = new Type("#text",{
+    strings:["AbuShahid"],
+    loop:true,
+    typeSpeed:100,
+    backSpeed:50,
+    backDelay:1000,
+
+});
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('feedback', function (event) {
+          if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  })()
